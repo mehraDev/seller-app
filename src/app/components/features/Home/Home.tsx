b7db1bf5a07e32d379cef8b75db84219423fb88b
mehraDev <126305156+mehraDev@users.noreply.github.com>
2023-06-25 06:27:19 +0530

    Home Adding heading

--- src/app/components/features/Home/Home.tsx
+++ src/app/components/features/Home/Home.tsx
@@ -2,15 +2,14 @@ import { getProfile } from "app/components/DashboardHost/services";
 import { IProfile } from "app/components/Signup/services";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
-import { Box,Text } from "ui/basic";
+import { Text } from "ui/basic";
 
-const ShopCard = styled(Box)`
+const ShopName = styled(Text)`
 `;
 
 
 const ShopCardComponent = () => {
   const [profile, setProfile] = useState<IProfile | 0>(0);
-  const shopDescription = "Your one-stop shop for trendy clothing.";
 
   useEffect(() => {
     async function fetchProfile() {
@@ -30,20 +29,12 @@ const ShopCardComponent = () => {
   }, []);
   
   const shopName = profile !== 0 ? profile.shopName : "My Store";
-  const shopType = profile !== 0 ? profile.shopType : "";
 
   return (
     <div>
-      <ShopCard j="between" p="2rem 1.5rem">
-      <Box fd="c" p="1.5rem 1rem">
-        <Text s="22" w={6} mb="0.5rem" c="#013f54" tt="cap">
+        <ShopName s="20" w={7} m={[2]} c="#013f54" tt="upp">
           {shopName}
-        </Text>
-        <Text s="12" w={6} mb="0.5rem" tt="cap">{shopType}</Text>
-        <Text mb="0.5rem">{shopDescription}</Text>
-      </Box>
-    </ShopCard>
-    
+        </ShopName>
     </div>
     
   );
