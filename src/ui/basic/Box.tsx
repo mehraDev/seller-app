d0117f77918091ac33699d098d9974cecdc26bdd
mehraDev <126305156+mehraDev@users.noreply.github.com>
2023-07-07 06:22:05 +0530

    Updating Ui/Box

--- src/ui/basic/Box.tsx
+++ src/ui/basic/Box.tsx
@@ -14,77 +14,83 @@
  */
 import styled from "styled-components";
 
-interface IBox {
-  /**
-   * Width of the box (e.g., "200px", "50%")
-   */
+export interface IBox {
   w?: string;
-  /**
-   * Height of the box (e.g., "200px", "50%")
-   */
   h?: string;
-  /**
-   * Padding of the box
-   */
-  p?: string;
-  /**
-   * Flex direction of the box ('r' for row, 'c' for column)
-   */
+  p?: string | number | number[];
+  m?: string | number | number[];
   fd?: "r" | "c";
-  /**
-   * Justify content of the box
-   */
-  j?: 'start' | 'end' | 'center' | 'between' | 'around' | 'evenly';
-  /**
-   * Align items of the box
-   */
-  a?: 'start' | 'end' | 'center' | 'stretch';
-  /**
-   * Margins of the box (array of numbers representing rem units)
-   */
-  m?: number[];
+  j?: "start" | "end" | "center" | "between" | "around" | "evenly";
+  a?: "start" | "end" | "center" | "stretch";
 }
 
 const Box = styled.div<IBox>`
   width: ${(props) => props.w || "100%"};
   height: ${(props) => props.h};
-  padding: ${props => props.p};
+  padding: ${(props) => {
+    const padding = props.p;
+    if (Array.isArray(padding)) {
+      return padding.map((item) => `${item}rem`).join(" ");
+    } else {
+      return padding;
+    }
+  }};
+  margin: ${(props) => {
+    const margin = props.m;
+    if (Array.isArray(margin)) {
+      return margin.map((item) => `${item}rem`).join(" ");
+    } else {
+      return margin;
+    }
+  }};
   display: flex;
-  flex-direction: ${(props) => (props.fd === 'c' ? 'column' : 'row')};
+  flex-direction: ${(props) => (props.fd === "c" ? "column" : "row")};
   justify-content: ${(props) => {
     switch (props.j) {
-      case 'start':
-        return 'flex-start';
-      case 'end':
-        return 'flex-end';
-      case 'center':
-        return 'center';
-      case 'between':
-        return 'space-between';
-      case 'around':
-        return 'space-around';
-      case 'evenly':
-        return 'space-evenly';
+      case "start":
+        return "flex-start";
+      case "end":
+        return "flex-end";
+      case "center":
+        return "center";
+      case "between":
+        return "space-between";
+      case "around":
+        return "space-around";
+      case "evenly":
+        return "space-evenly";
       default:
-        return 'flex-start';
+        return "flex-start";
     }
   }};
   align-items: ${(props) => {
     switch (props.a) {
-      case 'start':
-        return 'flex-start';
-      case 'end':
-        return 'flex-end';
-      case 'center':
-        return 'center';
-      case 'stretch':
-        return 'stretch';
+      case "start":
+        return "flex-start";
+      case "end":
+        return "flex-end";
+      case "center":
+        return "center";
+      case "stretch":
+        return "stretch";
       default:
-        return 'flex-start';
+        return "flex-start";
     }
   }};
-  margin: ${(props) =>
-    props.m ? props.m.map((item) => item + 'rem').join(' ') : ''};
 `;
 
 export default Box;
+
+interface IRow extends IBox {}
+
+const Row = styled(Box)<IRow>`
+  flex-direction: row;
+`;
+
+interface IColumn extends IBox {}
+
+const Col = styled(Box)<IColumn>`
+  flex-direction: column;
+`;
+
+export { Row, Col };
